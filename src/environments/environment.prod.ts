2227b12b428aa81ec0ef29d62e8748d4ff8714ce
Hansel <hanselcapillo@hotmail.com>
2020-05-02 15:17:47 -0500

    ignorara credenciales de firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyBxp5Tbq5YdAa1EiE9By2pgAvm7Nsv2dX8",
-    authDomain: "tracker-taxis-17eb4.firebaseapp.com",
-    databaseURL: "https://tracker-taxis-17eb4.firebaseio.com",
-    projectId: "tracker-taxis-17eb4",
-    storageBucket: "tracker-taxis-17eb4.appspot.com",
-    messagingSenderId: "345837688382",
-    appId: "1:345837688382:web:ca739721df9f94560ee36f",
-    measurementId: "G-ZC46KDL0N1",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    measurementId: "",
   },
 };
